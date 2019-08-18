5f82d63e7ee9a3141cbb9a46009c40a1fb3eead8
Marcel Widmer <marzelwidmer@gmail.com>
2019-08-18 11:00:40 +0200

    feat(release): Gradle Semantic Release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import net.thauvin.erik.gradle.semver.SemverIncrementBuildMetaTask
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 val logback_version: String by project
 val ktor_version: String by project
 val kotlin_version: String by project
@@ -7,13 +11,49 @@ group = "ch.keepclam"
 plugins {
     application
     kotlin("jvm") version "1.3.41"
-    id ("com.google.cloud.tools.jib") version "1.4.0"
-    id("de.gliderpilot.semantic-release") version "1.4.0"
+    id("com.google.cloud.tools.jib") version "1.4.0"
+//    id("de.gliderpilot.semantic-release") version "1.4.0"
+//    id("org.danilopianini.git-sensitive-semantic-versioning") version "0.2.2"
+//    id("net.vivin.gradle-semantic-build-versioning") version "4.0.0"
+//    id("de.maltsev.gradle.semanticrelease") version "0.3.4"
+//    id("com.github.moleksyuk.vcs-semantic-version") version "1.1.3"
+    id("net.thauvin.erik.gradle.semver") version "1.0.4"
+    id("com.github.gradle-git-version-calculator") version "1.1.0"
+//    id("com.github.ben-manes.versions").version("0.21.0")
+}
+
+
+tasks {
+    withType<Test> {
+        useTestNG()
+    }
+    "incrementBuildMeta"(SemverIncrementBuildMetaTask::class) {
+        doFirst {
+            buildMeta = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
+        }
+    }
+    "run"(JavaExec::class) {
+        doFirst {
+            println("Version: $version")
+        }
+        args = listOf("version.properties")
+    }
+}
+
+task(name = "fooTask"){
+    println( "Build: $semver.buildMeta")
+}
+
+
+task(name ="fooBar"){
+    println("Hello.... $version")
+
+    println(gitVersionCalculator.calculateVersion("prefix", true))
 }
 
 application {
     mainClassName = "io.ktor.server.netty.EngineMain"
-    applicationName = "ktor-webapp"
+    applicationName = "$rootProject.name"
     version = "$version"
     group = "$group"
 }
@@ -32,7 +72,6 @@ dependencies {
     compile("io.ktor:ktor-server-host-common:$ktor_version")
     // Test
     testCompile("io.ktor:ktor-server-tests:$ktor_version")
-
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
@@ -42,9 +81,9 @@ kotlin.experimental
 sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("testresources")
 
-// Jib Plugun
+// Jib Plugin
 jib.from.image = "openjdk:8-jre-alpine"
 jib.to.image = "c3smonkey/ktor-webapp:$version"  // Used for docker hub
-jib.container.jvmFlags = listOf( "-Djava.security.egd=file:/dev/./urandom")
+jib.container.jvmFlags = listOf("-Djava.security.egd=file:/dev/./urandom")
 jib.container.ports = listOf("8080")
 jib.container.user = "1000320000"
